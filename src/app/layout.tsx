569e8b130a2594a6c178b282d1bff4240e1b79cc
TheOfficialBug <kunalsrinidhi25@gmail.com>
2026-03-20 09:36:20 -0500

    added google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Besley } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* linking to our Google Analytics Dashboard
+      adding the tag manager script to the head of our document -> G-ZLCFW0RLZJ */}
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZLCFW0RLZJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZLCFW0RLZJ');
+          `}
+        </Script>
+      </head>
       <body
         suppressHydrationWarning
         className={`font-besley ${geistSans.variable} ${geistMono.variable} ${besley.variable} antialiased`}
